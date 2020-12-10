7e647c9c88fe88626a2e2ea28158f5dc3598ce23
Carlo Tongco <51468048+CarloTongco@users.noreply.github.com>
2020-12-10 19:38:51 +0800

    Added Shop Scene and Edited Main Menu

--- Jowa Simulator/Assets/Scripts/MainMenuScript.cs
+++ Jowa Simulator/Assets/Scripts/MainMenuScript.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
+    [SerializeField] private GameObject DebugPanel;
     // Start is called before the first frame update
     public void LoadGameScene()
     {
         Debug.Log("Loading Game Menu...");
         SceneManager.LoadScene("GameMenu");
+    }
+
+    public void LoadDebugPanel()
+    {
+        DebugPanel.SetActive(true);
     }
 
     public void ExitGame()
